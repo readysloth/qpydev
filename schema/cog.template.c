d473accbb17d95ad4a36c1e670c0c4e44536bb77
readysloth <readysloth@protonmail.com>
2021-07-21 18:04:43 +0300

    add(schema/cog.template.c): Deleted handwritten codegenerator in favor of cog

---
+++ schema/cog.template.c
@@ -0,0 +1,30 @@
+#include "qemu/osdep.h"
+#include "qapi/error.h"
+
+#include <Python.h>
+
+/*[[[cog
+    import json as j
+    import main as m
+
+    with open(m.DEV_SCHEMA_FILE, 'r') as sf:
+        SCHEMA = j.load(sf)
+
+    cog.outl(f"static Property {SCHEMA['name']}_properties[]{{")
+    for p in m.get_nested_schema(SCHEMA, 'properties'):
+        ptype = p['type'].upper()
+        pname = f"\"{p['name']}\""
+        pfield = p['field']
+        dev_name = m.get_device_class_name(SCHEMA)
+        default_value = ''
+
+        if 'default_value' in p:
+            default_value = ', ' + str(p['default_value'])
+
+        cog.outl(f"    DEFINE_PROP_{ptype}({pname}, {dev_name}, {pfield}{default_value}),")
+    cog.outl("    DEFINE_PROP_END_OF_LIST()")
+    cog.outl("}")
+  ]]]*/
+
+
+/*[[[end]]]*/
