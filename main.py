6e7f531e71fa0f0cc7110c73d25ce222218dda97
readysloth <readysloth@protonmail.com>
2021-07-22 18:12:18 +0300

    feat(main.py, templates/core.template.c): Added reflexy methods in main.py and class/instance codegen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import json
+import inspect
+import importlib.util
 import subprocess as subp
 
 from pathlib import Path
@@ -16,8 +18,26 @@ def get_device_class_name(schema: dict):
     return schema['name'].replace('_', ' ').title().replace(' ', '')
 
 
-def get_functions_from_schema(schema: dict):
-    pass
+def get_function_text_from(sub_schema: dict, method_name: str):
+    spec = importlib.util.spec_from_file_location("py_code", sub_schema["code"])
+    py_code = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(py_code)
+    return inspect.getsource(getattr(py_code, method_name)).split('\n')
+
+
+def get_python_prologue(schema: dict):
+    return f'Py_SetProgramName("{schema["name"]}");'
+
+
+def create_device_method_proto(schema: dict,
+                               ret_type: str,
+                               method_name: str,
+                               args: str,
+                               static=True):
+    method_proto = f"{ret_type} {schema['name']}_{method_name}({args})"
+    if static:
+        return f"static {method_proto}"
+    return method_proto
 
 
 DEV_SCHEMA_FILE = 'concrete_dev_schema.json'
@@ -28,9 +48,8 @@ if __name__ == '__main__':
     SCHEMA = sp.load_schema(sys.argv[1])
 
     with open(DEV_SCHEMA_FILE, 'w') as cds:
-        json.dump(SCHEMA, cds)
+        json.dump(SCHEMA, cds, indent=2)
 
 
     subp.run(['cog', '-d', '-o', SCHEMA["name"] + '.c', sys.argv[2]],
              env=ENV)
-    os.remove(DEV_SCHEMA_FILE)
--- templates/core.template.c
+++ templates/core.template.c
@@ -33,14 +33,14 @@
 
     cog.outl(f"static Property {SCHEMA['name']}_properties[]{{")
     for p in m.get_nested_schema(SCHEMA, 'properties'):
-        ptype = p['type'].upper()
+        ptype = p["type"].upper()
         pname = f"\"{p['name']}\""
-        pfield = p['field']
+        pfield = p["field"]
         dev_name = m.get_device_class_name(SCHEMA)
         default_value = ''
 
-        if 'default_value' in p:
-            default_value = ', ' + str(p['default_value'])
+        if "default_value" in p:
+            default_value = ', ' + str(p["default_value"])
 
         cog.outl(f"    DEFINE_PROP_{ptype}({pname}, {dev_name}, {pfield}{default_value}),")
     cog.outl("    DEFINE_PROP_END_OF_LIST()")
@@ -64,5 +64,88 @@
     for fname, ftype in m.get_nested_schema(SCHEMA, 'device').items():
         cog.outl(f"    {ftype} {fname};")
     cog.outl(f"}} {m.get_device_class_name(SCHEMA)};")
+  ]]]*/
+/*[[[end]]]*/
+
+
+/*[[[cog
+    #
+    # generating device and instance constructors and destructors
+    #
+
+    import json as j
+    import main as m
+
+    with open(m.DEV_SCHEMA_FILE, 'r') as sf:
+        SCHEMA = j.load(sf)
+
+    def offset_text_by(initiator, text):
+        return len(initiator)*' ' + text
+
+    class_init_func_proto = m.create_device_method_proto(SCHEMA, "void", "class_init", "ObjectClass *oc, void *data")
+    instance_init_func_proto = m.create_device_method_proto(SCHEMA, "void", "instance_init", "Object *obj")
+
+    cog.outl(f"{class_init_func_proto};")
+    cog.outl(f"{instance_init_func_proto};")
+    cog.outl()
+    cog.outl()
+
+    cog.outl(f"{class_init_func_proto}{{")
+
+    for entry in SCHEMA["class"]["schema"]["init"]["class_field_init"]:
+        cog.outl()
+        dev_cast = entry["dev_cast"]
+        cog.outl(f"    {dev_cast} *{dev_cast}_ptr = {dev_cast}(oc);")
+
+        for k,v in entry.items():
+            if k == "dev_cast":
+                continue
+            cog.outl(f"    {dev_cast}_ptr->{k} = {v};")
+    cog.outl("}")
+    cog.outl()
+    cog.outl()
+
+    cog.outl(f"{instance_init_func_proto}{{")
+
+    instance_init_source = m.get_function_text_from(SCHEMA['class'],
+                                                    SCHEMA['class']['schema']['instance_init'])
+
+    py_code_start = f"    char *py_code = "
+
+    cog.out(py_code_start + f'"{instance_init_source[0]}"\n')
+    for line in instance_init_source[1:-1]:
+        cog.out(offset_text_by(py_code_start, f'"{line}"\n'))
+    cog.out(offset_text_by(py_code_start, f'"{instance_init_source[-1]}";'))
+    cog.outl()
+
+    cog.outl("}")
+  ]]]*/
+/*[[[end]]]*/
+
+
+/*[[[cog
+    #
+    # generating device methods
+    #
+
+    import json as j
+    import main as m
+
+    with open(m.DEV_SCHEMA_FILE, 'r') as sf:
+        SCHEMA = j.load(sf)
+
+    read_func_proto = m.create_device_method_proto(SCHEMA, "uint64_t", "read", "void *opaque, hwaddr addr, unsigned size")
+    write_func_proto = m.create_device_method_proto(SCHEMA, "void", "write", "void *opaque, hwaddr addr, uint64_t data, unsigned size")
+
+    cog.outl(f"{read_func_proto};")
+    cog.outl(f"{write_func_proto};")
+    cog.outl()
+    cog.outl()
+    cog.outl(f"{read_func_proto}{{")
+    cog.outl("}")
+    cog.outl()
+    cog.outl()
+    cog.outl(f"{write_func_proto}{{")
+    cog.outl("}")
   ]]]*/
 /*[[[end]]]*/
