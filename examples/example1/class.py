fa8d7ce8ea83501cbb262c216df0a6e9a7275acc
readysloth <readysloth@protonmail.com>
2021-08-11 12:26:00 +0300

    feat(examples/example1/*): Made example device work (sort of)

--- examples/example1/class.py
+++ examples/example1/class.py
@@ -1,7 +1,9 @@
 
-def init_func(obj: bytes):
-    print(obj)
+def init_func(obj: memoryview, obj_info: dict):
+    print(obj_info)
+    for i,e in enumerate(b'hellow, how are you doink?'):
+        obj[len(obj)-4000+i] = e
 
 
-def finalize_func(obj: bytes):
+def finalize_func(obj: memoryview, obj_info: dict):
     print(obj)
